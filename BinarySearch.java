e7c63d12e5258449cabbab80b4c8ffbd04789475
Santhos kumar <52663775+santhoskumarayyappan@users.noreply.github.com>
2022-03-22 00:08:08 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -32,3 +32,20 @@ public int search() {
       }
       return left;
 }
+
+
+
+// Generic version
+// find index of last occurence of true for property in [left, right] T T T F F F
+public int search() {
+      int left = 0, right = n; // right = n or n - 1 such that [left, right] will have one True for property
+      while (left < right) {
+          int mid = left + (right - left) / 2;
+          if (!property(mid)) {
+              right = mid;
+          } else {
+              left = mid + 1;
+          }
+      }
+      return left - 1;
+}
